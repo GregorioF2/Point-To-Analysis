1bbe5739d1397f664a0ba3acce35f6a6bce0e254
Gregorio <gfreidin@dc.uba.ar>
2022-03-08 19:07:32 -0300

    Add files to start final tp

---
+++ src/main/java/tutorial/demo/FizzBuzz.java
@@ -0,0 +1,28 @@
+public class FizzBuzz {
+
+    public void printFizzBuzz(int k){
+        int intVal = 0;
+        if (k%15==0)
+            intVal +=1; // System.out.println("FizzBuzz");
+        Test firstTestVar = new Test();
+        Test2 secondTestVar = new Test2();
+        Test thirdTestVar = new Test();
+    }
+
+    public void fizzBuzz(int n){
+        for (int i=1; i<=n; i++)
+            printFizzBuzz(i);
+    }
+}
+
+class Test {
+    public int testMethod(int k) {
+        return k;
+    }
+}
+
+class Test2 {
+    public int testMethod(int k) {
+        return k;
+    }
+}
